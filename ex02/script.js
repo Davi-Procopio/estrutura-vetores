824ddd15a9f6294a06a463b089fd9e2805c8341e
Davi-Procopio <daviprocopio1234567@gmail.com>
2026-05-27 09:21:03 -0300

    VETOR-ALEATORIO

--- ex02/script.js
+++ ex02/script.js
@@ -1,10 +1,21 @@
-let vetor = [];
+let opcao;
 
-for (let i = 0; i < 7; i++) {
+do {
+    opcao = prompt('Digite "1" para gerar os números aleatórios:');
 
-    let numero = Math.floor(Math.random() * 10) + 1;
-    vetor[i] = numero;
+    if (opcao == "1") {
 
-}
+        let vetor = [];
 
-alert("Números gerados: " + vetor + ".");
+        for (let i = 0; i < 7; i++) {
+            let numero = Math.floor(Math.random() * 10) + 1;
+            vetor[i] = numero;
+        }
+
+        alert("Números gerados: " + vetor + ".");
+
+    } else {
+        alert("Opção inválida! Tente novamente.");
+    }
+
+} while (opcao != "1");
